bcc962f048ec40a6b4580a5ee660d32a3c60b4d9
felixtitonina <felixtitonina@outlook.com>
2024-01-14 01:46:15 -0300

    feat: add axios

--- src/modules/login/screens/LoginScreen.tsx
+++ src/modules/login/screens/LoginScreen.tsx
@@ -1,42 +1,58 @@
+import axios from "axios";
 import React, { useState } from "react";
 
 import Button from "../../../shared/buttons/button/Button";
+import SVGLogo from "../../../shared/icons /SVGLogo";
 import Input from "../../../shared/inputs/input/Input";
 import {
   BackgroundImage,
   ContainerLogin,
   ContainerLoginScreen,
   LimitedCotainer,
-  LogoImage,
   TitleLogin,
 } from "../styles/loginScreen.styles";
 
 const LoginScreen = () => {
-  const [username, setUsername] = useState("");
+  const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
-  const handleUsername = (event: React.ChangeEvent<HTMLInputElement>) => {
-    setUsername(event.target.value);
+  const handleEmail = (event: React.ChangeEvent<HTMLInputElement>) => {
+    setEmail(event.target.value);
   };
   const handlePassword = (event: React.ChangeEvent<HTMLInputElement>) => {
     setPassword(event.target.value);
   };
 
-  const handleLogin = () => {
-    alert(`${username} ${password}`);
+  const handleLogin = async () => {
+    const returnObject = await axios({
+      method: "POST",
+      url: "http://localhost:3000/auth",
+      data: {
+        email: "root@root.com",
+        password: "abc",
+      },
+    })
+      .then((result) => {
+        alert(`${result.data.accessToken}`);
+        return result;
+      })
+      .catch(() => {
+        alert(`usuario ou senha invalida`);
+      });
+    console.log("returnObject", returnObject);
   };
   return (
     <ContainerLoginScreen>
       <ContainerLogin>
         <LimitedCotainer>
-          <LogoImage src="./logo.png" />
+          <SVGLogo />
           <TitleLogin level={2} type="secondary">
             LOGIN
           </TitleLogin>
           <Input
             title="USUÁRIO"
             margin="32px 0px 0px"
-            onChange={handleUsername}
-            value={username}
+            onChange={handleEmail}
+            value={email}
           />
           <Input
             title="SENHA"
