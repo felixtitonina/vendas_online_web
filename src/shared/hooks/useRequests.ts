c2a5237ac993a47692184afef9d884aa4fc266a8
felixtitonina <felixtitonina@outlook.com>
2024-01-15 16:41:51 -0300

    feat: insert notification

--- src/shared/hooks/useRequests.ts
+++ src/shared/hooks/useRequests.ts
@@ -1,8 +1,12 @@
 import axios from "axios";
 import { useState } from "react";
 
+import { connectionAPIPost } from "../functions/connection/connectionAPI";
+import { useGlobalContext } from "./useGlobalContext";
+
 export const useRequests = () => {
   const [loading, setLoadding] = useState(false);
+  const { setNotification } = useGlobalContext();
 
   const getRequest = async (url: string) => {
     setLoadding(true);
@@ -28,19 +32,15 @@ export const useRequests = () => {
    * @returns
    */
   // eslint-disable-next-line @typescript-eslint/no-explicit-any
-  const postRequest = async (url: string, body: any) => {
+  const postRequest = async (url: string, body: unknown) => {
     setLoadding(true);
-    const returnData = await axios({
-      method: "post",
-      url: url,
-      data: body,
-    })
+    const returnData = await connectionAPIPost(url, body)
       .then((result) => {
-        alert(`Login sucesso!!!`);
-        return result.data;
+        setNotification("Senha válida", "success");
+        return result;
       })
-      .catch(() => {
-        alert(`Error`);
+      .catch((error: Error) => {
+        setNotification(error.message, "error");
       });
     setLoadding(false);
     return returnData;
