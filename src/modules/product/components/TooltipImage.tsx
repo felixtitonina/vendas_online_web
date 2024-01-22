ce13dd3b5abd9ce0266a03032fa870b910976499
felixtitonina <felixtitonina@outlook.com>
2024-01-21 22:32:24 -0300

    feat: insert catogory table product

---
+++ src/modules/product/components/TooltipImage.tsx
@@ -0,0 +1,18 @@
+import { Tooltip } from "antd";
+
+import { ProductType } from "../../../shared/types/ProductType";
+
+interface TooltipImageProps {
+  product: ProductType;
+}
+
+const TooltipImage = ({ product }: TooltipImageProps) => {
+  return (
+    <Tooltip title={product.name}>
+      {" "}
+      <span> {product.id} </span>{" "}
+    </Tooltip>
+  );
+};
+
+export default TooltipImage;
