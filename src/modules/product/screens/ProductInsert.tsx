5096ab5d4ae3441133b4ca0900d1788f868f4dea
felixtitonina <felixtitonina@outlook.com>
2024-01-23 18:39:20 -0300

    feat: insert product in backend

--- src/modules/product/screens/ProductInsert.tsx
+++ src/modules/product/screens/ProductInsert.tsx
@@ -1,11 +1,13 @@
-import { Select } from "antd";
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 
 import Button from "../../../shared/components/buttons/button/Button";
 import Input from "../../../shared/components/inputs/input/Input";
+import Select from "../../../shared/components/inputs/select/Select";
 import Screen from "../../../shared/components/screen/Screen";
-import { URL_CATEGORY } from "../../../shared/constants/urls";
+import { URL_CATEGORY, URL_PRODUCT } from "../../../shared/constants/urls";
+import { InsertProduct } from "../../../shared/dtos/InsertProduct.dto";
 import { MethodsEnum } from "../../../shared/enums/methods.enum";
+import { connectionAPIPost } from "../../../shared/functions/connection/connectionAPI";
 import { useDataContext } from "../../../shared/hooks/useDataContext";
 import { useRequests } from "../../../shared/hooks/useRequests";
 import { ProductRoutesEnum } from "../enums/productRoutes.enum";
@@ -25,39 +27,83 @@ const listBreadcrumb = [
 ];
 
 const ProductInsert = () => {
+  const [product, setProduct] = useState<InsertProduct>({
+    name: "",
+    price: 0,
+    image: "",
+  });
   const { categories, setCategories } = useDataContext();
   const { request } = useRequests();
-
+  // console.log("categories", categories);
+  console.log("product", product);
   useEffect(() => {
     if (categories.length === 0) {
       request(URL_CATEGORY, MethodsEnum.GET, setCategories);
     }
   }, []);
 
+  const handleInsertProduct = () => {
+    connectionAPIPost(URL_PRODUCT, product);
+  };
+
+  const onChange = (
+    event: React.ChangeEvent<HTMLInputElement>,
+    nameObject: string,
+  ) => {
+    setProduct({
+      ...product,
+      [nameObject]: event.target.value,
+    });
+  };
+
+  const onChangePrice = (event: React.ChangeEvent<HTMLInputElement>) => {
+    setProduct({
+      ...product,
+      price: Number(event.target.value),
+    });
+  };
+
   const handleChange = (value: string) => {
-    console.log("handleChange", value);
+    // console.log("handleChange", value);
+    setProduct({ ...product, categoryId: Number(value) });
   };
 
   return (
     <Screen listBreadcrumb={listBreadcrumb}>
       <LimitedContainer>
-        <Input margin="0px 0px 16px 0px" title="Nome" placeholder="Nome" />
         <Input
+          onChange={(event) => onChange(event, "name")}
+          value={product.name}
+          margin="0px 0px 16px 0px"
+          title="Nome"
+          placeholder="Nome"
+        />
+        <Input
+          onChange={(event) => onChange(event, "image")}
+          value={product.image}
           margin="0px 0px 16px 0px"
           title="URL imagem"
           placeholder="URL da imagem"
         />
-        <Input margin="0px 0px 16px 0px" title="Preço" placeholder="Preço" />
+        <Input
+          onChange={(event) => onChangePrice(event)}
+          value={product.price}
+          margin="0px 0px 16px 0px"
+          title="Preço"
+          placeholder="Preço"
+        />
         <Select
-          defaultValue="selecionar"
-          style={{ width: "50%", margin: "0px 0px 16px 0px" }}
+          title="Categoria"
+          margin="0px 0px 32px 0px"
           onChange={handleChange}
           options={categories.map((category) => ({
             value: `${category.id}`,
             label: `${category.name}`,
           }))}
         />
-        <Button type="primary">Inserir produto</Button>
+        <Button onClick={handleInsertProduct} type="primary">
+          Inserir produto
+        </Button>
       </LimitedContainer>
     </Screen>
   );
