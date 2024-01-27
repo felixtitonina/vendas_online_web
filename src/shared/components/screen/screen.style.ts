3c3e121c80c20c84e215414e04058528d1e3ebcb
felixtitonina <felixtitonina@outlook.com>
2024-01-26 23:21:39 -0300

    feat: fix background-color

--- src/shared/components/screen/screen.style.ts
+++ src/shared/components/screen/screen.style.ts
@@ -1,8 +1,7 @@
 import styled from "styled-components";
 
 export const ScreenContainer = styled.div`
-  /* display: flex; */
-  /* justify-content: flex-end; */
+  background-color: white;
   padding: 32px;
   margin: 32px;
   width: calc(100% - 368px);
