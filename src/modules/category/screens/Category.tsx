e9fd52fc782e320e8f89d10b23fbda73dd6398e2
felixtitonina <felixtitonina@outlook.com>
2024-01-27 17:40:25 -0300

    feat: create table category

--- src/modules/category/screens/Category.tsx
+++ src/modules/category/screens/Category.tsx
@@ -1,14 +1,77 @@
+import { Input, Table } from "antd";
+import { ColumnType } from "antd/es/table";
+import { useNavigate } from "react-router-dom";
+
+import Button from "../../../shared/components/buttons/button/Button";
 import Screen from "../../../shared/components/screen/Screen";
+import { DisplayFlexJustifyBetween } from "../../../shared/components/styles/display.styled";
+import { LimitedContainer } from "../../../shared/components/styles/limited.styled";
+import { CategoryType } from "../../../shared/types/CategoryType";
+import { CategoryRoutesEnum } from "../enums/categoryRoutes.enum";
 import { useCategory } from "../hooks/useCategory";
+
+const { Search } = Input;
+const columns: ColumnType<CategoryType>[] = [
+  {
+    title: "Id",
+    dataIndex: "id",
+    key: "id",
+    render: (text) => <a>{text}</a>,
+  },
+  {
+    title: "Nome",
+    dataIndex: "name",
+    key: "name",
+    sorter: (a, b) => a.name.localeCompare(b.name),
+    render: (text) => <a>{text}</a>,
+  },
+  {
+    title: "Produtos",
+    dataIndex: "amountProducts",
+    key: "amountProducts",
+    render: (text) => <a>{text}</a>,
+  },
+];
+
+const listBreadcrumb = [
+  {
+    name: "HOME",
+  },
+  {
+    name: "CATEGORIAS",
+  },
+];
 
 const Category = () => {
   const { categories } = useCategory();
+  const navigate = useNavigate();
+
+  const handleOnClickCategory = () => {
+    navigate(CategoryRoutesEnum.CATEGORY_INSERT);
+  };
+
+  const handleOnSearch = (value: string) => {
+    console.log(value);
+  };
   return (
-    <Screen>
-      <div>Categorias</div>
-      {categories.map((category) => (
-        <div key={category.name}>{category.name}</div>
-      ))}
+    <Screen listBreadcrumb={listBreadcrumb}>
+      <DisplayFlexJustifyBetween margin="0px 0px 16px 0px">
+        <LimitedContainer width={240}>
+          <Search
+            placeholder="Buscar categoria"
+            onSearch={handleOnSearch}
+            enterButton
+          />
+        </LimitedContainer>
+
+        <LimitedContainer width={120}>
+          <Button type="primary" onClick={handleOnClickCategory}>
+            Inserir
+          </Button>
+        </LimitedContainer>
+      </DisplayFlexJustifyBetween>
+
+      <Table columns={columns} dataSource={categories} />
     </Screen>
   );
 };
--- src/modules/product/screens/Product.tsx
+++ src/modules/product/screens/Product.tsx
@@ -5,6 +5,8 @@ import { useNavigate } from "react-router-dom";
 
 import Button from "../../../shared/components/buttons/button/Button";
 import Screen from "../../../shared/components/screen/Screen";
+import { DisplayFlexJustifyBetween } from "../../../shared/components/styles/display.styled";
+import { LimitedContainer } from "../../../shared/components/styles/limited.styled";
 import Table from "../../../shared/components/table/Table";
 import { URL_PRODUCT } from "../../../shared/constants/urls";
 import { MethodsEnum } from "../../../shared/enums/methods.enum";
@@ -15,11 +17,6 @@ import { ProductType } from "../../../shared/types/ProductType";
 import CategoryColumn from "../components/CategoryColumn";
 import TooltipImage from "../components/TooltipImage";
 import { ProductRoutesEnum } from "../enums/productRoutes.enum";
-import {
-  BoxButtons,
-  LimiteSizeButton,
-  LimiteSizeInput,
-} from "../styles/product.style";
 
 const columns: TableProps<ProductType>["columns"] = [
   {
@@ -88,21 +85,21 @@ const Product = () => {
 
   return (
     <Screen listBreadcrumb={listBreadcrumb}>
-      <BoxButtons>
-        <LimiteSizeInput>
+      <DisplayFlexJustifyBetween margin="0px 0px 16px 0px">
+        <LimitedContainer width={240}>
           <Search
             placeholder="Buscar produto"
             onSearch={onSearch}
             enterButton
           />
-        </LimiteSizeInput>
+        </LimitedContainer>
 
-        <LimiteSizeButton>
+        <LimitedContainer width={120}>
           <Button type="primary" onClick={handleOnClickInsert}>
             Inserir
           </Button>
-        </LimiteSizeButton>
-      </BoxButtons>
+        </LimitedContainer>
+      </DisplayFlexJustifyBetween>
       <Table columns={columns} dataSource={productsFiltered} />
     </Screen>
   );
